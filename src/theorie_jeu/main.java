f2464484fd8251d2cdd763a9b01fd8e0cea8b734
Pierre Cabanis <pierre.cabanis@etu.univ-lyon1.fr>
2019-06-24 10:20:48 +0200

    [UP]

--- src/theorie_jeu/main.java
+++ src/theorie_jeu/main.java
@@ -50,11 +50,92 @@ public class main {
         int res1vs1 = playGame(game1vs1);
         int res1vs2 = playGame(game1vs2);
         int res2vs2 = playGame(game2vs2);
+        int matrix[][] = new int [3][3];
+        matrix [0][0] = resRvsR;
+        matrix [0][1] = resRvs1;
+        matrix [0][2] = resRvs2;
+        matrix [1][0] = -resRvs1;
+        matrix [1][1] = res1vs1;
+        matrix [1][2] = res1vs2;
+        matrix [2][0] = -resRvs2;
+        matrix [2][1] = -res1vs2;
+        matrix [2][2] = res2vs2;
 
-        System.out.println(resRvsR + "    " + resRvs1 + "    " + resRvs2);
-        System.out.println(-resRvs1 + "    " + res1vs1 + "    " + res1vs2);
-        System.out.println(-resRvs2 + "    " + -res1vs2 + "    " + res2vs2);
+        printMatrix(matrix);
 
+        switch (trouverPrudente(matrix)){
+            case 0:
+                System.out.println("Strategie prudente est StratRandom");
+                break;
+            case 1:
+                System.out.println("Strategie prudente est Strat1");
+                break;
+            case 2:
+                System.out.println("Strategie prudente est Strat2");
+                break;
+            default:
+                assert (false);
+                break;
+        }
+
+        int[] tab = new int[2];
+        tab = trouverNash(matrix);
+
+        System.out.println(tab[0]);
+        System.out.println(tab[1]);
+
+    }
+
+    private static int trouverPrudente(int[][] matrix) {
+        int ligne = -1;
+        int total;
+        int max;
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        max = 0;
+        for(int i=0;i<rows;i++){
+            total = 0;
+            for(int j=0;j<columns;j++){
+                total += matrix[i][j];
+            }
+            if(total > max){
+                max = total;
+                ligne = i;
+            }
+        }
+
+
+
+        return ligne;
+    }
+
+    private static int[] trouverNash(int[][] matrix) {
+
+        int min;
+        int total;
+        int ligne = -1;
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        min = -1;
+        for(int i=0;i<columns;i++){
+            total = 0;
+            for(int j=0;j<rows;j++){
+                total += matrix[i][j];
+            }
+            if(total > min || min == -1){
+                min = total;
+                ligne = i;
+            }
+        }
+
+        int[] tab = new int[2];
+        tab[0] = trouverPrudente(matrix);
+        tab[1] = ligne;
+        return tab;
     }
 
 
@@ -84,6 +165,24 @@ public class main {
         //System.out.println("victoire j2 " + nbVictoireJ2);
 
         return nbVictoireJ1 - nbVictoireJ2;
+    }
+
+    public static void printMatrix(int[][] m){
+        try{
+            int rows = m.length;
+            int columns = m[0].length;
+            String str = "|\t";
+
+            for(int i=0;i<rows;i++){
+                for(int j=0;j<columns;j++){
+                    str += m[i][j] + "\t";
+                }
+
+                System.out.println(str + "|");
+                str = "|\t";
+            }
+
+        }catch(Exception e){System.out.println("Matrix is empty!!");}
     }
 
 }
